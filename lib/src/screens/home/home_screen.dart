68d7b3c6a325c50a94787109ec7d14eb2a458d3d
hun73877 <hun230402@gmail.com>
2022-09-10 16:10:11 +0900

    안드 버전 수정

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -4,12 +4,17 @@ import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:tmtt/src/screens/base/base_scaffold.dart';
 import 'package:tmtt/src/screens/home/home_controller.dart';
+import 'package:tmtt/src/util/my_logger.dart';
+import 'package:tmtt/src/util/my_navigator.dart';
 
 
 class HomeScreen extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
+      onPressedAosBackButton: () {
+        Log.d('onPressedAosBackButton');
+      },
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
