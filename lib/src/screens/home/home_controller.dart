1e1619ecf7acc6fce10ed9190511011a2d48a06f
Mark Choi <fantasysa@gmail.com>
2022-09-17 22:29:43 +0900

    bio 테스트 출력.

--- lib/src/screens/home/home_controller.dart
+++ lib/src/screens/home/home_controller.dart
@@ -1,13 +1,9 @@
 
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_insta/flutter_insta.dart';
 import 'package:get/get.dart';
 import 'package:tmtt/src/screens/base/base_get_controller.dart';
 import 'package:tmtt/src/util/my_logger.dart';
-
-import 'package:http/http.dart' as http;
 
 class HomeBinding implements Bindings {
   @override
@@ -19,6 +15,8 @@ class HomeBinding implements Bindings {
 class HomeController extends BaseGetController {
 
   late final inputController = TextEditingController();
+
+  var userNameObs = ''.obs;
 
   @override
   void onInit() {
@@ -32,26 +30,24 @@ class HomeController extends BaseGetController {
 
   void searchInstaUser() async {
 
-    String userName = inputController.text;
-    Log.d(userName);
+    String a = inputController.text;
+    Log.d(a);
+    FlutterInsta flutterInsta = FlutterInsta();
+    await flutterInsta.getProfileData(a); //instagram username
 
-    // FlutterInsta flutterInsta = FlutterInsta();
-    // await flutterInsta.getProfileData(userName); //instagram username
+    print("Username : ${flutterInsta.username}");
+    print("Followers : ${flutterInsta.followers}");
+    print("Folowing : ${flutterInsta.following}");
 
-    String url = "https://www.instagram.com/$userName";
-    String fullUrl = "$url/?__a=1&_a_d=dis";
-    Log.d(fullUrl);
+    userNameObs.value = flutterInsta.bio;
 
-    var res = await http.get(Uri.parse(Uri.encodeFull(fullUrl)));
 
-    Log.d(res);
-    Log.d(res.body);
-
-    Log.d(json.decode(res.body));
-
-    // print("Username : ${flutterInsta.username}");
-    // print("Followers : ${flutterInsta.followers}");
-    // print("Folowing : ${flutterInsta.following}");
+    /// 1. parse a document String
+    // BeautifulSoup bs = BeautifulSoup("https://www.instagram.com/$a");
+    // print(bs.body);
+    // var found= bs.find('profile_pic_url_hd');
+    // print(found);
+// use BeautifulSoup.fragment(html_doc_string) if you parse a part of html
   }
 
   @override
