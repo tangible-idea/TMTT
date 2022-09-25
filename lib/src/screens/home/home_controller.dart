ad99a76b45b7e43c39debe5264c0b03b59de3937
hun73877 <hun230402@gmail.com>
2022-09-25 18:00:38 +0900

    이미지 다운로드 작업 취소

--- lib/src/screens/home/home_controller.dart
+++ lib/src/screens/home/home_controller.dart
@@ -1,9 +1,12 @@
 
+
 import 'package:flutter/material.dart';
+
 import 'package:flutter_insta/flutter_insta.dart';
 import 'package:get/get.dart';
 import 'package:tmtt/src/screens/base/base_get_controller.dart';
 import 'package:tmtt/src/util/my_logger.dart';
+
 
 class HomeBinding implements Bindings {
   @override
@@ -29,7 +32,6 @@ class HomeController extends BaseGetController {
   }
 
   void searchInstaUser() async {
-
     String userName = inputController.text;
     Log.d(userName);
 
@@ -37,34 +39,14 @@ class HomeController extends BaseGetController {
     await flutterInsta.getProfileData(userName); //instagram username
     String a = inputController.text;
     Log.d(a);
-
-    print("Username : ${flutterInsta.username}");
-    print("Followers : ${flutterInsta.followers}");
-    print("Folowing : ${flutterInsta.following}");
-    print("imgurl : ${flutterInsta.imgurl}");
-    print("Username : ${flutterInsta.username}");
-    print("Followers : ${flutterInsta.followers}");
-    print("Folowing : ${flutterInsta.following}");
-
     userNameObs.value = flutterInsta.bio;
-    // String url = "https://www.instagram.com/$userName";
-    // String fullUrl = "$url/?__a=1&_a_d=dis";
-    // Log.d(fullUrl);
-    //
-    // var res = await http.get(Uri.parse(Uri.encodeFull(fullUrl)));
-    //
-    // Log.d(res);
-    // Log.d(res.body);
-    //
-    // Log.d(json.decode(res.body));
 
-
-    /// 1. parse a document String
-    // BeautifulSoup bs = BeautifulSoup("https://www.instagram.com/$a");
-    // print(bs.body);
-    // var found= bs.find('profile_pic_url_hd');
-    // print(found);
-// use BeautifulSoup.fragment(html_doc_string) if you parse a part of html
+    Log.d(
+      flutterInsta.username + '\n' +
+      flutterInsta.followers + '\n' +
+      flutterInsta.following + '\n' +
+      flutterInsta.imgurl
+    );
   }
 
   @override
