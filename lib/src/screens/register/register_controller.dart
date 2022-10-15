67a21ec26fb553acd31259f8841e80ed32910b9a
Mark <mark.choi@tangibleidea.net>
2022-10-14 20:06:35 -0700

    구글 로그인 완료.

--- lib/src/screens/register/register_controller.dart
+++ lib/src/screens/register/register_controller.dart
@@ -1,9 +1,11 @@
 
 
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_insta/flutter_insta.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tmtt/firebase/fire_store.dart';
 import 'package:tmtt/pages.dart';
 import 'package:tmtt/src/constants/local_storage_keys.dart';
@@ -22,10 +24,12 @@ class RegisterBinding implements Bindings {
 
 class RegisterController extends BaseGetController {
 
+
   late final inputController = TextEditingController();
 
   @override
-  void onInit() { }
+  void onInit() {
+  }
 
   @override
   void onClose() { }
@@ -34,6 +38,23 @@ class RegisterController extends BaseGetController {
     MyNav.pushReplacementNamed(
       pageName: PageName.home,
     );
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
 
--- lib/src/screens/register/register_screen.dart
+++ lib/src/screens/register/register_screen.dart
@@ -1,18 +1,24 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tmtt/src/resources/styles/my_color.dart';
+import 'package:tmtt/src/resources/styles/txt_style.dart';
 import 'package:tmtt/src/screens/base/base_app_bar.dart';
 import 'package:tmtt/src/screens/base/base_scaffold.dart';
 import 'package:tmtt/src/screens/register/register_controller.dart';
 import 'package:tmtt/src/util/app_space.dart';
 import 'package:tmtt/src/util/my_logger.dart';
+import 'package:tmtt/src/util/my_snackbar.dart';
 import 'package:tmtt/src/widgets/bottom_button.dart';
 
 import '../../../generated/assets.dart';
 
 class RegisterScreen extends GetView<RegisterController> {
+
+
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
@@ -36,19 +42,28 @@ class RegisterScreen extends GetView<RegisterController> {
 
               // logo here
               Center(child: Image.asset(Assets.imagesTmttLogoWhite36)),
-              AppSpaces.verticalSpace100,
+              AppSpaces.verticalSpace90,
 
               // explanation image
               SvgPicture.asset(Assets.imagesExplainationSocial),
-              AppSpaces.verticalSpace40,
+              AppSpaces.verticalSpace20,
 
-              const Text("Get anonymous messages on any kind of \r\n"+
-                  "social media apps such as Instagram!"),
-
+              Text("Get anonymous messages on any kind of \r\n"+
+                  "social media apps such as Instagram!",
+                style: MyTextStyle.body.copyWith(color: Colors.white),
+                textAlign: TextAlign.center),
               AppSpaces.verticalSpace40,
 
               // social login buttons
-              InkWell(child: SvgPicture.asset(Assets.imagesBtnLoginGoogle),),
+              InkWell(onTap: () async {
+                try {
+                  UserCredential credential= await controller.signInWithGoogle();
+                  MySnackBar.show(title: 'Login', message: credential.user?.email.toString());
+                }catch(e) {
+                  MySnackBar.show(title: 'Exception', message: e.toString());
+                }
+              },
+                child: SvgPicture.asset(Assets.imagesBtnLoginGoogle),),
               AppSpaces.verticalSpace10,
               SvgPicture.asset(Assets.imagesBtnLoginFacebook),
 
