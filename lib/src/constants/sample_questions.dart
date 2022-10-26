8e8f65f3cdf7aa77fe5ca00feb72c4c520f54c26
Mark <mark.choi@tangibleidea.net>
2022-10-26 22:18:22 +0900

    Add random sample questions.

--- lib/src/constants/sample_questions.dart
+++ lib/src/constants/sample_questions.dart
@@ -3,10 +3,23 @@
 class Samples {
   static var questions_en_1 = [
     "Ask me anything, anonymously!",
-    "Tell me our biggest throwback and I'll try to guess who you are 😎",
-    "If you're real copy and paste the last text you got and I'll guess who it was from 🫣",
+    "Tell me our biggest throwback and I'll try to guess who you are. 😎",
+    "If you're real copy and paste the last text you got and I'll guess who it was from. 🙃",
     "Describe me in 3 words, anonymously.",
     "Tell me what you REALLY think about me!",
     "Tell me your crush, anonymously.",
+    "Tell me your favorite fruit 😋, I'll guess who you are.",
+    "Tell me your favorite dish 😋, I'll guess who you are.",
+  ];
+
+  static var questions_kr_1 = [
+    "아무거나 질문해주세요~ 익명이라 누군지 몰라요!",
+    "우리의 가장 임팩트있는 추억 말해주면 누군지 맞혀봄! 😎",
+    "지금 마지막으로 복사된 문장을 붙여넣기해서 보내면 누군지 맞혀볼게요! 🙃",
+    "세 단어로 나를 표현한다면? 익명이라 누군지 모름.",
+    "나에 대해서 어떻게 생각해? 익명이라 누군지 모름.",
+    "첫사랑에 대해서 말해줄사람. 익명이라 누군지 모름.",
+    "가장 좋아하는 과일 말해주면 누군지 맞춰볼게요 😋",
+    "가장 좋아하는 음식 말해주면 누군지 맞춰볼게요 😋",
   ];
 }
