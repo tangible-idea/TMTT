c022a81ca5884971c5e97e36ad533e87d2778848
hun73877 <hun230402@gmail.com>
2022-11-06 20:16:40 +0900

    언어설정 추가 - 태국어 추가

--- lib/src/resources/languages/languages.dart
+++ lib/src/resources/languages/languages.dart
@@ -1,13 +1,21 @@
 import 'package:get/get.dart';
+import 'package:tmtt/src/resources/languages/languages_th.dart';
 
 import 'language_en.dart';
 import 'language_kr.dart';
+
+class LocaleKey {
+  static const ko_KR = "ko_KR";
+  static const en_US = "en_US";
+  static const th_TH = "th_TH"; // 태국어
+}
 
 class Languages extends Translations {
 
   @override
   Map<String, Map<String, String>> get keys => {
-    'ko_KR': translations_kr,
-    'en_US': translations_en,
+    LocaleKey.ko_KR: translations_kr,
+    LocaleKey.en_US: translations_en,
+    LocaleKey.th_TH: translations_th,
   };
 }
