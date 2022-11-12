9c3a5dcad31afdf1cba54350f4e21c7f959f1fb0
hun73877 <hun230402@gmail.com>
2022-11-12 22:03:26 +0900

    remove dotenv dependencies

--- lib/src/constants/dot_env_store.dart
+++ lib/src/constants/dot_env_store.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tmtt/src/constants/app_secret.dart';
 
 class DotEnvStore {
-  static String get revenueCatGoogleSdkKey => dotenv.env['REVENUE_CAT_GOOGLE_SDK_KEY'] ?? '';
-  static String get revenueCatAppleSdkKey => dotenv.env['REVENUE_CAT_APPLE_SDK_KEY'] ?? '';
+  static String get revenueCatGoogleSdkKey => AppSecret.REVENUE_CAT_GOOGLE_SDK_KEY;
+  static String get revenueCatAppleSdkKey => AppSecret.REVENUE_CAT_APPLE_SDK_KEY;
 }
