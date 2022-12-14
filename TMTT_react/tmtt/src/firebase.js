fb9f5eadddb044840cd6531eeb956cbf9c92e1e0
Mark <mark.choi@tangibleidea.net>
2022-12-14 10:31:27 +0700

    Typo.

--- TMTT_react/tmtt/src/firebase.js
+++ TMTT_react/tmtt/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: env.REACT_APP_API_KEY,
-    authDomain: env.REACT_APP_AUTH_DOMAIN,
-    projectId: env.REACT_APP_PROJECT_ID,
-    storageBucket: env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: env.REACT_APP_APP_ID,
-    measurementId: REACT_APP_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
 // Initialize Firebase
